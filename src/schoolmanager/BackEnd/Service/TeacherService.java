0d8dcdc2070ac93ca12614a0579b8c9d6d3f55c7
Zebda Yacine <zebdaadam1996@gmail.com>
2022-04-02 11:41:16 +0100

    add level Service

---
+++ src/schoolmanager/BackEnd/Service/TeacherService.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schoolmanager.BackEnd.Service;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import static schoolmanager.BackEnd.DataBaseConnection.con;
+import schoolmanager.BackEnd.Model.Teacher;
+import schoolmanager.BackEnd.Results;
+
+/**
+ *
+ * @author Zed Yacine
+ */
+public class TeacherService {
+
+    public static Results.Rstls addTeacher(Teacher teacher) {
+        try {
+            PreparedStatement stm = (PreparedStatement) con.prepareStatement(""
+                    + "insert into teacher (firstName,lastName,phone,workePlace)"
+                    + " values (?,?,?,?)");
+            stm.setString(1, teacher.getFirstName());
+            stm.setString(2, teacher.getLastName());
+            stm.setString(3, teacher.getPhoneTeacher());
+            stm.setString(4, teacher.getWorkePlace());
+            stm.executeUpdate();
+            stm.close();
+            return Results.Rstls.OBJECT_INSERTED;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return Results.Rstls.OBJECT_NOT_INSERTED;
+        }
+    }
+
+    public static Results.Rstls updateTeacher(Teacher teacher) {
+        try {
+            PreparedStatement stm = (PreparedStatement) con.prepareStatement("UPDATE "
+                    + " teacher SET firstName = ?"
+                    + ", lastName = ? ,phone = ? ,workePlace = ? "
+                    + " WHERE id = ? ");
+            stm.setString(1, teacher.getFirstName());
+            stm.setString(2, teacher.getLastName());
+            stm.setString(3, teacher.getPhoneTeacher());
+            stm.setString(4, teacher.getWorkePlace());
+            stm.setLong(5, teacher.getId());
+            stm.executeUpdate();
+            stm.close();
+            return Results.Rstls.OBJECT_UPDATED;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return Results.Rstls.OBJECT_NOT_UPDATED;
+        }
+    }
+
+     public static Results.Rstls deleteTeacher(Teacher teacher) {
+        try {
+            PreparedStatement stm = (PreparedStatement) con.prepareStatement("DELETE FROM "
+                    + " teacher WHERE id = ?");
+            stm.setLong(1, teacher.getId());
+            stm.executeUpdate();
+            stm.close();
+            return Results.Rstls.OBJECT_DELETED;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return Results.Rstls.OBJECT_NOT_DELETED;
+        }
+    }
+
+    public static ObservableList<Teacher> getAllTeachers() {
+        String query;
+        query = "SELECT * FROM teacher ";
+        ObservableList<Teacher> listTeachers = FXCollections.observableArrayList(new Teacher());
+        listTeachers.remove(0);
+        try {
+            PreparedStatement ps = (PreparedStatement) con.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Teacher student = new Teacher();
+                student.setId(rs.getLong("id"));
+                student.setFirstName(rs.getString("firstName"));
+                student.setLastName(rs.getString("lastName"));
+                student.setPhoneTeacher(rs.getString("phone"));
+                student.setWorkePlace(rs.getString("workePlace"));
+                listTeachers.add(student);
+            }
+            rs.close();
+            ps.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return listTeachers;
+    }
+
+    public static Teacher searchStudentByName(Teacher teacher) {
+        String query;
+        query = "SELECT * FROM student where firstName='" + teacher.getFirstName() + "' and "
+                + " lastName='" + teacher.getLastName() + "'";
+        Teacher tchr = new Teacher();
+        try {
+            PreparedStatement ps = (PreparedStatement) con.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                tchr.setId(rs.getLong("id"));
+                tchr.setFirstName(rs.getString("firstName"));
+                tchr.setLastName(rs.getString("lastName"));
+                tchr.setPhoneTeacher(rs.getString("phone"));
+                tchr.setWorkePlace(rs.getString("workePlace"));
+            }
+            rs.close();
+            ps.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return tchr;
+    }
+
+}
