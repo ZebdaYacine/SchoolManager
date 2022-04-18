1adc08e31bf5d3b27fa4d1994f57cbfb05c95f8d
harran95 <kadri.harran@gmail.com>
2022-04-18 23:01:10 +0100

    add offre.fxml

--- src/schoolmanager/BackEnd/Controller/MainController.java
+++ src/schoolmanager/BackEnd/Controller/MainController.java
@@ -57,36 +57,36 @@ public class MainController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
+        stat.setStyle("-fx-background-color : #dfe3ee;"
+                + "-fx-border-color:  #0099cc #0099cc #dfe3ee #0099cc;"
+                + "-fx-padding: 0");
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/schoolmanager/FrontEnd/layout/stat.fxml"));
+            main.setCenter(null);
+            if (root != null) {
+                main.setCenter(root);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public void changePane(JFXButton btn1, JFXButton btn2, JFXButton btn3, JFXButton btn4, JFXButton btn5, String rsc) throws IOException {
         btn1.setStyle("-fx-background-color :  linear-gradient(#081018 0%, #283f54 70%);"
+                + "-fx-border-color:  #0099cc #0099cc #dfe3ee #0099cc;"
                 + "    -fx-padding: 0");
         btn2.setStyle("-fx-background-color : transparent;");
         btn3.setStyle("-fx-background-color : transparent;");
         btn4.setStyle("-fx-background-color : transparent;");
         btn5.setStyle("-fx-background-color : transparent;");
-        Parent root = FXMLLoader.load(getClass().getResource("/FrontEnd/" + rsc + ".fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/FrontEnd/layout/" + rsc + ".fxml"));
         main.setCenter(null);
         if (root != null) {
             main.setCenter(root);
         }
     }
 
-    @FXML
-    private void btn1(ActionEvent event) throws IOException {
-
-    }
-
-    @FXML
-    private void btn2(ActionEvent event) throws IOException {
-
-    }
-
-    @FXML
-    private void btn3(ActionEvent event) throws IOException {
-    }
 
     private void btn4(ActionEvent event) throws IOException {
 
@@ -143,15 +143,6 @@ public class MainController implements Initializable {
 
     }
 
-    @FXML
-    private void Facture_Purchase(ActionEvent event) throws IOException {
-
-    }
-
-    @FXML
-    private void Facture_Sale(ActionEvent event) throws IOException {
-
-    }
 
     @FXML
     private void close(MouseEvent event) {
@@ -184,6 +175,26 @@ public class MainController implements Initializable {
             Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
         }
 
+    }
+
+    @FXML
+    private void stat(ActionEvent event) {
+    }
+
+    @FXML
+    private void paiement(ActionEvent event) {
+    }
+
+    @FXML
+    private void presence(ActionEvent event) {
+    }
+
+    @FXML
+    private void seance(ActionEvent event) {
+    }
+
+    @FXML
+    private void offre(ActionEvent event) {
     }
 
 }
